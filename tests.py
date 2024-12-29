9333aae39effd4f195426afcd51daf74adc6ccac
Claudio Laas <claudiolaas@gmail.com>
2024-12-29 21:10:13 +0100

    cleanup

--- tests.py
+++
@@ -1,6 +0,0 @@
-from data_fetcher import DataFetcher, CryptoDataFetcher, AlpacaDataFetcher
-
-data_fetcher = CryptoDataFetcher()
-
-df = data_fetcher.get_data(start="2021-01-01", end="2021-01-03", market="BTC/USDT", step="1h")
-print(df.head())
